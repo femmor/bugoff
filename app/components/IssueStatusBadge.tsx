51dfdecbdbeeceea9c7a1d1f9255b741497bdfd1
Omolara1950 <fegomson@gmail.com>
2024-01-25 10:19:48 -0600

    Build the issue status badge

---
+++ app/components/IssueStatusBadge.tsx
@@ -0,0 +1,34 @@
+import { Status } from "@prisma/client";
+import { Badge } from "@radix-ui/themes";
+
+interface Props {
+  status: Status;
+}
+
+const statusMap: Record<
+  Status,
+  {
+    label: string;
+    color: "orange" | "blue" | "green";
+  }
+> = {
+  OPEN: {
+    label: "Open",
+    color: "orange",
+  },
+  IN_PROGRESS: {
+    label: "In Progress",
+    color: "blue",
+  },
+  CLOSED: {
+    label: "Closed",
+    color: "green",
+  },
+};
+
+const IssueStatusBadge = ({ status }: Props) => {
+  return (
+    <Badge color={statusMap[status].color}>{statusMap[status].label}</Badge>
+  );
+};
+export default IssueStatusBadge;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,13 +1,60 @@
-import { Button } from "@radix-ui/themes";
+import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
+import prisma from "@/prisma/client";
+import IssueStatusBadge from "../components/IssueStatusBadge";
+import { Status } from "@prisma/client";
 
-const IssuesPage = () => {
+interface Issue {
+  id: number;
+  title: string;
+  description: string;
+  status: string;
+  createdAt: Date;
+}
+
+const IssuesPage = async () => {
+  const issues: Issue[] = await prisma.issue.findMany();
+
   return (
-    <div>
-      Issues Page
-      <Button>
-        <Link href="/issues/new">Create Issue</Link>
-      </Button>
+    <div className="">
+      <div className="mb-5">
+        <Button>
+          <Link href="/issues/new">Create Issue</Link>
+        </Button>
+      </div>
+
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues?.map((issue: Issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                {issue.title}
+                <div className="block md:hidden">
+                  <IssueStatusBadge status={issue.status as Status} />
+                </div>
+              </Table.Cell>
+
+              <Table.Cell className="hidden md:table-cell">
+                <IssueStatusBadge status={issue.status as Status} />
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </div>
   );
 };
